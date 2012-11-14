5941664bf3b40cc7837ab5aa837f4b9d0ffd713e
D3VON <dmcbrid1@binghamton.edu>
2012-11-14 03:35:50 -0500

    slight changes to notes

--- SelectionSort.c
+++ SelectionSort.c
@@ -5,19 +5,18 @@
  * Wikipedia says:
  * - An in-place comparison sort (destroys original order of original array).
  * - It has O(n2) time complexity, making it inefficient on large lists,
- * and generally performs worse than the similar insertion sort.
+ *   and generally performs worse than the similar insertion sort.
  * - Selection sort is noted for its simplicity, and also has performance
- * advantages over more complicated algorithms in certain situations,
- * particularly where auxiliary memory is limited.
+ *   advantages over more complicated algorithms in certain situations,
+ *   particularly where auxiliary memory is limited.
  *
  * Really nice animated gif on wikipedia here:
  * http://upload.wikimedia.org/wikipedia/commons/b/b0/Selection_sort_animation.gif
  *
- *
- * You can compile this with something like
+ * You can compile this with something like:
  *     gcc SelectionSort.c -o SelectionSort (to compile as C)
- *    (c++ SelectionSort.c -0 SelectionSort to compile as C++)
- * and run it with something like
+ *     c++ SelectionSort.c -o SelectionSort (to compile as C++)
+ * ...and run it with something like:
  *     ./SelectionSort < datafile.txt
  */
 #include <stdio.h>
